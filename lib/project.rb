1d339d9efea32fead1d9e0db9feb7b6f2b218563
Mark Drehmann <markdrehmann@gmail.com>
2020-03-07 18:55:02 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -9,7 +9,7 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.backed_projects << self unless backer.backed_projects.includes?(self)
+    backer.backed_projects << self unless backer.backed_projects.include?(self)
   end
 
 end
